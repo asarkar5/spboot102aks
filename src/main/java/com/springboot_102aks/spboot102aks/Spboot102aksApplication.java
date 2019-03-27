040e9f0ea1fdabc9f03fce37d2c03822f354b5ff
asarkar5 <ashoke.k.sarkar@gmail.com>
2019-03-27 19:31:52 -0400

    Initial commit

---
+++ src/main/java/com/springboot_102aks/spboot102aks/Spboot102aksApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_102aks.spboot102aks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spboot102aksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spboot102aksApplication.class, args);
+	}
+
+}
